c55d9843e7ace59b129363194d04c80b1ffe5330
Hack Reactor Student <student@hackreactor.com>
2017-06-03 15:40:28 -0700

    3pm Updates

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -12,27 +12,27 @@ window.searchYouTube = searchYouTube;
 // buildApiRequest('GET',
 
 
-  var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+//   var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
 
-  $.ajax({
-    url: youtubeUrl,
-    data: options,
-    success: callback,
-    dataType: null
+//   $.ajax({
+//     url: youtubeUrl,
+//     data: options,
+//     success: callback,
+//     dataType: null
 
-  });
+//   });
 
-search: function(query) {
-  var options = this.fetch( {
-    var options: {
-      part: 'snippet',
-      key: window.YOUTUBE_API_KEY,
-      q: query,
-      max: 5,
-      type: 'video',
-      videoEmbeddable: 'true',
-  }
-});
+// search: function(query) {
+//   var options = this.fetch( {
+//     var options: {
+//       part: 'snippet',
+//       key: window.YOUTUBE_API_KEY,
+//       q: query,
+//       max: 5,
+//       type: 'video',
+//       videoEmbeddable: 'true',
+//   }
+// });
   
 //   }) 
 // }
