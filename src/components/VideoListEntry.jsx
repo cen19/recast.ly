0b049533e6596292d52e2643ff20a0279af74fe8
cen19 <wilsoncen@gmail.com>
2017-06-02 19:37:21 -0700

    Refactoring VideoListEntry to dynamically list things

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,23 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+class VideoListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="video-list-entry">
+        <div className="media-left media-middle">
+        </div>
+          <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+        <div className="media-body">
+          <div className="video-list-entry-title">Video Title</div>
+          <div className="video-list-entry-detail">Video Description</div>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
