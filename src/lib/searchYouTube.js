24786dbb572bdc186e3c64d6dcb0f34078628811
Hack Reactor Student <student@hackreactor.com>
2017-06-03 16:10:48 -0700

    Search YouTube tests passed

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,38 +1,36 @@
-var searchYouTube = (options, callback) => {
+var searchYouTube = ({key, query, max = 10}, callback) => {
   // options parameter 
     // most likely takes in the search query
   // callback is a function
     // passes on the search Query into the getRequest
 
+  $.get('https://www.googleapis.com/youtube/v3/search',
+    {
+      part: 'snippet',
+      key: key,
+      q: query,
+      maxResults: max,
+      type: 'video',
+      videoEmbeddable: 'true',
+    }
+  )
   
+  .done(({items}) => {
+    if (callback) {
+      callback(items);
+    }
+  });
+
+  // var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+  // $.ajax({
+  //   url: youtubeUrl,
+  //   data: search,
+  //   success: callback,
+  //   dataType: null
+
+  // });
+
 };
 
 window.searchYouTube = searchYouTube;
-
-// buildApiRequest('GET',
-
-
-//   var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
-
-//   $.ajax({
-//     url: youtubeUrl,
-//     data: options,
-//     success: callback,
-//     dataType: null
-
-//   });
-
-// search: function(query) {
-//   var options = this.fetch( {
-//     var options: {
-//       part: 'snippet',
-//       key: window.YOUTUBE_API_KEY,
-//       q: query,
-//       max: 5,
-//       type: 'video',
-//       videoEmbeddable: 'true',
-//   }
-// });
-  
-//   }) 
-// }
